8a8f6bd59d566ebf5ac83cc52fb0f2822151d9e5
sanarung <sanarunglimbu77@gmail.com>
2026-06-18 10:12:36 +0545

    heading change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>let discuss about the project</h1>
+      <h1>let discuss about the our e-commerce-project</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
