42303bf1d3ca9be44270123ae9e87927979336f3
sanarung <sanarunglimbu77@gmail.com>
2026-06-19 15:55:31 +0545

    Add header component

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
+import MainLayout from "./layouts/MainLayout";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/About" element={<AboutPage/>} />
+        <Route path="/" element={<MainLayout />} />
+        <Route index element={<HomePage />} />
+        <Route path="/About" element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   );
